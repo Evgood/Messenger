58c88ac0a21de9ca8961df4de6f7e9a869ed3a89
Evgeniy Gorokhov <gorohovev@list.ru>
2021-12-06 22:30:00 +0300

    Обновлнен компонет SettingCard

--- src/components/settingCard/settingCard.js
+++
@@ -1,5 +0,0 @@
-import Handlebars from 'handlebars/dist/handlebars.runtime';
-import template from './settingCard.hbs';
-import './settingCard.scss';
-
-Handlebars.registerPartial('settingCard', template);
---
+++ src/components/settingCard/settingCard.ts
@@ -0,0 +1,17 @@
+import Block from '../../utils/Block';
+/* @ts-ignore */
+import template from './button.hbs';
+
+type Props = Record<string, any>;
+
+class SettingCard extends Block {
+    constructor(props: Props = {}) {
+        super("div", props);
+    }
+
+    render() {
+        return this.setTemplate(template, this.props);
+    }
+}
+
+export default SettingCard;
