06608ce4ee7c7346f9f1fd860b3757569206f13b
Evgeniy Gorokhov <gorohovev@list.ru>
2022-01-06 22:56:12 +0300

    Обновлен роут страницы Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Props } from '../../types';
 import Block from '../../utils/Block'
-import { inputFocus, inputBlur } from '../../utils/events'
+import { inputFocus, inputBlur, signIn, goToRegister } from '../../utils/events'
+// import AuthController from '../../controllers/auth';
 
 import Form from '../../module/form/form';
 import Input from '../../components/input/input';
@@ -46,17 +47,15 @@ class Login extends Block {
             formTitle: 'Вход',
             footerFormDesc: 'Нет аккаунта?',
             footerFormLinkName: 'Регистрация',
-            footerFormLinkUrl: '/register',
+            events: {
+                submit: signIn,
+                click: goToRegister,
+            }
         }, innerInputs)
 
-        super(
-            'div',
-            {
-                ...props,
-                form
-            }
-        );
+        super('div', { ...props, form });
     }
+
 
     render() {
         return this.setTemplate(template, this.props);
