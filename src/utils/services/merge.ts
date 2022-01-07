5eae5c105244587f27fe2aaaa0a586de3b4bbd9d
Evgeniy Gorokhov <gorohovev@list.ru>
2022-01-07 20:40:50 +0300

    Добавлен Store

---
+++ src/utils/services/merge.ts
@@ -0,0 +1,23 @@
+import { Props } from 'types';
+
+const merge = (lhs: Props, rhs: Props) => {
+    for (let p in rhs) {
+        if (!rhs.hasOwnProperty(p)) {
+            continue;
+        }
+
+        try {
+            if (rhs[p].constructor === Object) {
+                rhs[p] = merge(lhs[p], rhs[p]);
+            } else {
+                lhs[p] = rhs[p];
+            }
+        } catch (e) {
+            lhs[p] = rhs[p];
+        }
+    }
+
+    return lhs;
+}
+
+export default merge;
