5e1fdd29d4c9bc1447bcee537cbd4878bd615c9e
Evgeniy Gorokhov <gorohovev@list.ru>
2022-01-11 14:04:06 +0300

    Добавлены тесты для Router

--- src/utils/Router.spec.ts
+++ src/utils/Router.spec.ts
@@ -1,15 +1,64 @@
-// import { JSDOM } from 'jsdom';
+import { JSDOM } from 'jsdom';
 import { expect } from 'chai';
 import Router from './Router';
 
+class Block {
+    getElement() {
+        const div = document.createElement('div');
+        div.setAttribute('data-test-id', 'testComponent');
+
+        return div;
+    }
+
+    dispatchMountComponent() { }
+
+    deleteElement() { }
+}
+
+const selectComponent = () => {
+    return document.querySelector('[data-test-id="testComponent"]');
+};
+
 describe('Router', () => {
     beforeEach(() => {
-        (global as any).window = {};
+        const dom = new JSDOM('<div id="root"></div>', {
+            url: 'http://localhost:3000',
+        });
+
+        global.document = dom.window.document;
+        (global as any).window = dom.window;
     });
 
     it('should be sigletone', () => {
         const router = Router;
 
         expect(Router).to.eq(router);
+    });
+
+    describe('.use', () => {
+        it('should return Router instance', () => {
+            const router = Router;
+            const result = router.use('/', Block);
+
+            expect(result).to.eq(router);
+        });
+    });
+
+    describe('.start', () => {
+        it('should render current page', () => {
+            const router = Router;
+            router.use('/', Block).start();
+
+            expect(selectComponent()).not.to.be.null;
+        });
+    });
+
+    describe('.go', () => {
+        it('should render new component', () => {
+            const router = Router;
+            router.use('/test', Block).go('/test');
+
+            expect(selectComponent()).not.to.be.null;
+        });
     });
 });
